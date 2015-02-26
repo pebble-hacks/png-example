990070a6e7569ec6c0d8aedbee6cdf6eb28f8126
C-D-Lewis <Bonsitm@gmail.com>
2015-02-26 14:32:21 -0800

    Add project

---
+++ src/png_example.c
@@ -0,0 +1,43 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static GBitmap *s_bitmap;
+static BitmapLayer *s_bitmap_layer;
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DOG);
+
+  s_bitmap_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+}
+
+static void main_window_unload(Window *window) {
+  bitmap_layer_destroy(s_bitmap_layer);
+  gbitmap_destroy(s_bitmap);
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_set_fullscreen(s_main_window, true);
+  window_set_background_color(s_main_window, GColorRed);
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
